129adca007780cfa7488f045f00b8d95e91679fd
Irvin Dinkins <irvdude44@gmail.com>
2024-01-13 12:24:43 -0500

    completed css display

---
+++ CSS/css-viz-and-layout/css-display/app.js
@@ -0,0 +1,9 @@
+const blockDiv = document.querySelector(".block-level");
+
+const hide = () => {
+  blockDiv.style.display = "none";
+};
+
+const show = () => {
+  blockDiv.style.display = "block";
+};
